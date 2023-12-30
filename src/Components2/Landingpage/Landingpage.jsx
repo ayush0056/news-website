841bae5f470ff2ae257209350c2dece3c7e2fe61
ayush0056 <154000766+ayush0056@users.noreply.github.com>
2023-12-31 00:03:21 +0530

    Update Landingpage.jsx

--- src/Components2/Landingpage/Landingpage.jsx
+++ src/Components2/Landingpage/Landingpage.jsx
@@ -4,8 +4,9 @@ import {Outlet,Link} from 'react-router-dom'
 export const Landingpage = () => { 
   return (
     <div className='containers6'>
-        <div className='header'>
+        <div className='header1'>
             <div className='title1'><h1>The Times Of XYZ</h1></div>
+            <Link to="/login-signup" className='login'>Login-Signup</Link>
           <Outlet />
         </div>
     </div>
